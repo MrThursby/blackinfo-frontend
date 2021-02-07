5372a88183d378d75bc251a48053d08e6b96ef3d
MrThursby <smykov.03@ya.ru>
2021-02-07 11:37:51 +0300

    text --Не является публичной офертой-- added in footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,24 +1,25 @@
 <template>
-    <footer class="bg-dark text-light py-4">
-        <div class="container">
-            <div class="row">
-                <div class="col-auto pb-3 pb-sm-0">
-                    <h5>BlackInfo</h5>
-                    <ul class="nav flex-column flex-sm-row mb-0">
-                        <li v-for="(item, index) of menu" class="nav-item" :key="index">
-                            <nuxt-link :to="'/pages/'+item.id" class="nav-link text-light p-0 pr-sm-3">
-                              {{ item.title }}
-                            </nuxt-link>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-12 col-sm text-sm-right align-self-end">
-                    <!--&copy; Все права защищены - 2020-->
-                  Соискателей в базе: {{ count }}
-                </div>
-            </div>
+  <footer class="bg-dark text-light py-4">
+    <div class="container">
+      <div class="row">
+        <div class="col-auto pb-3 pb-md-0">
+          <h5>BlackInfo</h5>
+          <ul class="nav flex-column flex-sm-row mb-3">
+            <li v-for="(item, index) of menu" class="nav-item" :key="index">
+              <nuxt-link :to="'/pages/'+item.id" class="nav-link text-light p-0 pr-sm-3">
+                {{ item.title }}
+              </nuxt-link>
+            </li>
+          </ul>
+          <span class="text-white">Не является публичной офертой</span>
         </div>
-    </footer>
+        <div class="col-12 col-sm text-md-right align-self-end">
+          <!--&copy; Все права защищены - 2020-->
+          Соискателей в базе: {{ count }}
+        </div>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <script>
@@ -36,8 +37,8 @@ export default {
 
 <style lang="scss" scoped>
 .nav {
-    .nav-link {
-        padding: 2px 10px;
-    }
+  .nav-link {
+    padding: 2px 10px;
+  }
 }
 </style>
